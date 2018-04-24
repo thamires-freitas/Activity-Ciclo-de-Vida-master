4227f8c7635b53ea0d7d2e1817b9223033acc66d
arqdsis <arqdsis@mocli10230.animaedu.intranet>
2018-04-24 18:45:48 -0300

    Initial commit

---
+++ app/src/main/java/com/example/danie/myapplication/activity_b.java
@@ -0,0 +1,87 @@
+package com.example.danie.myapplication;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class activity_b extends Activity {
+
+    private String mActivityName;
+    private TextView mStatusView;
+    private TextView mStatusAllView;
+    private StatusTracker mStatusTracker = StatusTracker.getInstance();
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_b);
+        mActivityName = getString(R.string.activity_b);
+        mStatusView = (TextView)findViewById(R.id.status_view_b);
+        mStatusAllView = (TextView)findViewById(R.id.status_view_all_b);
+        mStatusTracker.setStatus(mActivityName, getString(R.string.on_create));
+        Utils.printStatus(mStatusView, mStatusAllView);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mStatusTracker.setStatus(mActivityName, getString(R.string.on_start));
+        Utils.printStatus(mStatusView, mStatusAllView);
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        mStatusTracker.setStatus(mActivityName, getString(R.string.on_restart));
+        Utils.printStatus(mStatusView, mStatusAllView);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mStatusTracker.setStatus(mActivityName, getString(R.string.on_resume));
+        Utils.printStatus(mStatusView, mStatusAllView);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mStatusTracker.setStatus(mActivityName, getString(R.string.on_pause));
+        Utils.printStatus(mStatusView, mStatusAllView);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mStatusTracker.setStatus(mActivityName, getString(R.string.on_stop));
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mStatusTracker.setStatus(mActivityName, getString(R.string.on_destroy));
+        mStatusTracker.clear();
+    }
+
+    public void startDialog(View v) {
+        Intent intent = new Intent(activity_b.this, DialogActivity.class);
+        startActivity(intent);
+    }
+
+    public void startActivityA(View v) {
+        Intent intent = new Intent(activity_b.this, activity_a.class);
+        startActivity(intent);
+    }
+
+    public void startActivityC(View v) {
+        Intent intent = new Intent(activity_b.this, activity_c.class);
+        startActivity(intent);
+    }
+
+    public void finishActivityB(View v) {
+        activity_b.this.finish();
+    }
+
+}
